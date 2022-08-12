a2448454479093994fc379b63089737557dba5c2
lakpahana <agslakpahana@gmail.com>
2022-08-12 14:09:01 +0530

    final commit

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.LineHeightSpan
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +20,30 @@ class MainActivity : AppCompatActivity() {
         calcButton.setOnClickListener {
             val weight = weightText.text.toString()
             val height = heightText.text.toString()
-            val bmi = weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
-            //to round off to two decimal places
-            val bmi2Digits = String.format("%.2f",bmi).toFloat()
-            displayResult(bmi2Digits)
+            if (validateInput(weight,height)) {
+                val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat() / 100))
+                //Log.i("BMI",bmi.toString())
+                //to round off to two decimal places
+                val bmi2Digits = String.format("%.2f", bmi).toFloat()
+                displayResult(bmi2Digits)
+            }
+        }
+    }
+
+    private fun validateInput(weight:String?,height:String?):Boolean{
+        return when{
+            weight.isNullOrEmpty()->{
+                Toast.makeText(this,"Weight is empty",Toast.LENGTH_SHORT).show()
+                return false
+            }
+            height.isNullOrEmpty()->{
+                Toast.makeText(this,"Height is empty",Toast.LENGTH_SHORT).show()
+                return false
+            }
+            else ->{
+                return true
+            }
+
         }
     }
 
@@ -32,6 +56,26 @@ class MainActivity : AppCompatActivity() {
 
         var resultText = ""
         var color = 0
+        when {
+            bmi<18.50 -> {
+                resultText = "Underweight"
+                color = R.color.under_weight
+            }
+            bmi in 18.50..24.99->{
+                resultText = "Healthy"
+                color = R.color.normal
+            }
+            bmi in 25.00..29.99->{
+                resultText = "Overweight"
+                color = R.color.over_weight
+            }
+            bmi > 29.99 ->{
+                resultText = "Obese"
+                color = R.color.obese
+            }
+        }
+        resultDescription.text = resultText
+        resultDescription.setTextColor(ContextCompat.getColor(this,color))
     }
 
 }
